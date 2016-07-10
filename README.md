5219b83e59f0727527c708cc51206876bf3efd57
sheryl <sherylhsu@agtopnet.com>
2016-07-10 17:54:21 +0800

    [Title] Slackbot with jieba init commit [Symptom] [Root Cause] [Solution]

---
+++ README.md
@@ -0,0 +1,2 @@
+
+Reference: [Jieba](https://github.com/fxsjy/jieba) and [Slackbot](https://github.com/lins05/slackbot)
---
+++ slackbot_settings.py
@@ -0,0 +1,5 @@
+API_TOKEN = "xxxxxxxxx"
+DEFAULT_REPLY = "Sorry but I didn't understand you"
+PLUGINS = [
+    'slackbot.plugins'
+]
